0718ddd10f3020de83979976b130b5f05d6fc67b
pokhanov <1296798652@qq.com>
2023-03-08 14:54:10 +0800

    笑脸点阵+蜂鸣器

--- laughingFace.v
+++ laughingFace.v
@@ -1,10 +1,20 @@
-module cryingFace(clk, hang, gre);
+module laughingFace(success, clk, hang, gre, beep);
+input success;
 input clk;
 output reg[7:0] hang, gre;
+output reg beep;
 reg[2:0] s1;
+reg[15:0] tt;
 
 always@(posedge clk)	//时钟上升沿触发
 begin
+	if(success==1) begin
+		
+		if(tt==10) begin beep=~beep;
+							  tt=0;
+					  end
+		else tt=tt+1;
+
     if(s1==7) s1=0;else s1=s1+1;
 			case(s1)
 			0:begin hang=8'b01111111;gre=8'b00000000;end	//笑脸
@@ -16,7 +26,14 @@ begin
 			6:begin hang=8'b11111101;gre=8'b00100100;end
 			7:begin hang=8'b11111110;gre=8'b00011000;end
 			default:hang=8'b11111111;
-			endcase;   
+			endcase;
+			
+	end
+	
+	else begin hang=8'b11111111;
+				  gre =8'b00000000;
+		  end
+			
 end
 
 
